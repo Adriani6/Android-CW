590291d7d90b157e38588b825d4877f8f50aca9e
Adrian Iwaszkiewicz <adrian@adriani6.co.uk>
2017-03-07 03:04:25 +0000

    Fixed Different App Phases

---
+++ app/src/main/java/com/a1502689/adriani6/cw/SandwichBuilder.java
@@ -0,0 +1,15 @@
+package com.a1502689.adriani6.cw;
+
+import android.app.Activity;
+
+/**
+ * Created by Adriani6 on 3/7/2017.
+ */
+
+public class SandwichBuilder {
+
+    public SandwichBuilder(Activity a)
+    {
+        a.setContentView(R.layout.bread_screeen);
+    }
+}
